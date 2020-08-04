f779ac022c441f55d1395c5eb3d2d08c0a2b5011
Everton Spader <everton.spader@gmail.com>
2020-08-04 20:39:41 +0200

    Setup of login.html and signup.html
    * Edits in accounts/urls.py
    * Setup of base.html

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^$', views.HomePage.as_view(), name='home'),
+    re_path(r'accounts/', include('accounts.urls', namespace='accounts')),
+    re_path(r'accounts/', include('django.contrib.auth.urls')),
 ]
