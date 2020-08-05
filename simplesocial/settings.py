c96a08b3c9a05e3d054e90d96457f018815cdee4
Everton Spader <everton.spader@gmail.com>
2020-08-05 08:36:56 +0200

    Included test/thanks pages for login and logout

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -124,5 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES = [os.path.join(BASE_DIR, 'static')
-]
+STATIC_FILES = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
